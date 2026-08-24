e7edb30928b85b3fd2cc43279c10dd77161ea1ff
stormy <anya.iz7@yandex.ru>
2026-08-24 13:45:35 +0300

    fix: remove unused DeleteMapping import in ItemController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
